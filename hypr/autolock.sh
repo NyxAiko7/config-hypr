183721c4870d8b23f1b0b20200bb4ff3fc413cac
Silver wolf <vfvfv028@gmail.com>
2025-11-29 01:56:20 +0300

    fix

--- hypr/autolock.sh
+++ hypr/autolock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 swayidle -w \
-    timeout 60 'hyprlock' \
-    timeout 61 'hyprctl dispatch dpms off' \
+    timeout 300 'hyprlock' \
+    timeout 301 'hyprctl dispatch dpms off' \
     resume 'hyprctl dispatch dpms on' \
     before-sleep 'hyprlock' &
